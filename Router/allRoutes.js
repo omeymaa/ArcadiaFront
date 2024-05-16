2b74810ced44919d0f6a8fc144ebd2d7c773cd90
Omeyma <omeyma.maatoug@gmail.com>
2024-05-16 15:37:14 +0200

    adding header and navigation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/le-parc", "Le parc", "/pages/le-parc.html"),
+    new Route("/les-animaux", "Les animaux", "/pages/les-animaux.html"),
+    new Route("/infos-pratiques", "Infos pratiques", "/pages/infos-pratiques.html"),
+    new Route("/activites", "Activités", "/pages/activites.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Ar";
+export const websiteName = "Arcadia";
---
+++ pages/activites.html
@@ -0,0 +1,1 @@
+<h1>Activités</h1>
